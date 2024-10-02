0bdc9f9b561ea7f239933f7ea564b0df85167692
Alexsam56 <152397048+Alexsam69@users.noreply.github.com>
2024-10-03 01:03:38 +0530

    Update maze.py

--- maze.py
+++ maze.py
@@ -1,29 +1,29 @@
 import sys
 
 class Node():
-    def __init__(self,state,parent,action):
-        self.state=state
-        self.parent=parent
-        self.action=action
+    def __init__(self, state, parent, action):
+        self.state = state
+        self.parent = parent
+        self.action = action
         
 class StackFrontier():
     def __init__(self):
-        self.frontier=[]
+        self.frontier = []
         
-    def add(self,node):
+    def add(self, node):
         self.frontier.append(node)
     
-    def contain_state(self,state):
-        return any(node.state==state for node in self.frontier)
+    def contain_state(self, state):
+        return any(node.state == state for node in self.frontier)
     
     def empty(self):
-        return len(self.frontier)==0
+        return len(self.frontier) == 0
     
     def remove(self):
-        if self.empty:
+        if self.empty():
             raise Exception("empty frontier")  
         else:
-            node=self.frontier[-1]
+            node = self.frontier[-1]
             self.frontier = self.frontier[:-1]
             return node
         
@@ -31,10 +31,8 @@ class QueueFrontier(StackFrontier):
     
     def remove(self):
         if self.empty():
-            raise Exception("empty Frontier")
-            
+            raise Exception("empty frontier")
         else:
-            node=self.frontier[0]
-            self.frontier=self.frontier[1:]
+            node = self.frontier[0]
+            self.frontier = self.frontier[1:]
             return node
-            
